96f54619f397a643eb5189bc6a77b84be65a48bf
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-07-04 20:01:14 +0530

    added contact form html and css style files

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ app.engine('handlebars',exphbs());
 app.set('view engine' , 'handlebars');
 
 //static folder
-app.use('./public' , express.static(path.join(__dirname,'public')));
+app.use('/public' , express.static(path.join(__dirname,'public')));
 
 //body parser Midleware
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/',(req,res) => {
-    res.render('contact');
+    res.render('contact', {layout: false});
 
 });
 
