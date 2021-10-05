b25903e95ee85d68511d99f1e80c15b8d9f7d78b
hnsemage <himasha.semage@gmail.com>
2021-10-06 02:48:14 +0530

    added a value to index.js

--- views/index.js
+++ views/index.js
@@ -1,4 +1,4 @@
-const months = ["January", "February", "March", "April", "May", "June", "July","August"];
+const months = ["January", "February", "March", "April", "May", "June", "July","August","September","October"];
 
 const random = Math.floor(Math.random() * months.length);
 console.log(random, months[random]);
