fbbbfd0366db1ed67c201c16dd8c55b5aca1d62b
hnsemage <himasha.semage@gmail.com>
2021-10-06 02:51:27 +0530

    added an array to index.js

--- views/index.js
+++ views/index.js
@@ -1,4 +1,5 @@
 const months = ["January", "February", "March", "April", "May", "June", "July","August","September","October"];
+const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 const random = Math.floor(Math.random() * months.length);
 console.log(random, months[random]);
