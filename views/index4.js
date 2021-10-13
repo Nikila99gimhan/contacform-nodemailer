41241a3e76700f450f9d2c3113570242e93d90d9
thuthimarian <74374348+thuthimarian@users.noreply.github.com>
2021-10-14 01:11:14 +0530

    Update index4.js

--- views/index4.js
+++ views/index4.js
@@ -1,5 +1,5 @@
-const country = ["Maldives", "Bali", "Hawaii", "Korea", "UK", "Singapore", "Iceland","France","Canada","Hongkong"];
+const languages = ["Spanish","English","Hindi","Chinese"];
 
 
-const random = Math.floor(Math.random() * country.length);
-console.log(random, country[random]);
+const random = Math.floor(Math.random() * laguages.length);
+console.log(random, languages[random]);
